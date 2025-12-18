04e29cc0953df252459a11677c430a878e7dec12
llrebecca21 <llrebecca21@gmail.com>
2025-12-18 12:12:19 -0800

    Update file name to match updated function name.

--- R/grad_loc_HBEST_fast.R
+++ R/grad_loc_HBEST_fast.R
@@ -1,7 +1,7 @@
 #' Calculate the gradient of the conditional log posterior distribution of the \eqn{\pmb{e}^{(r)}} from Model A.
 #' 
 #' @description
-#' `gradiente_modelA_fast` function calculates the conditional posterior for the `r`th \eqn{\beta} coefficients under Model B.
+#' `grad_loc_HBEST_fast` function calculates the conditional posterior for the `r`th \eqn{\beta} coefficients under Model B.
 #' 
 #' @param sumPsi  The `r`th column of the column sum across `Psi`.
 #' @param Psi The `r`th `Psi` matrix.
@@ -12,6 +12,6 @@
 #'
 #' @return A vector containing the gradient for the conditional posterior of \eqn{\beta}.
 #'
-gradiente_modelA_fast <- function(er, sumPsi, Psi, y, Sigma_e, Psi_ab) {
+grad_loc_HBEST_fast <- function(er, sumPsi, Psi, y, Sigma_e, Psi_ab) {
   -sumPsi - (er / Sigma_e) + colSums(Psi * c(y / exp(Psi_ab + Psi %*% er)))
 }
