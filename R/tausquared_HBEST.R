29fce5123e1f4f6d599c20cbd3048cc0143bf49f
llrebecca21 <llrebecca21@gmail.com>
2025-12-17 18:26:04 -0800

    update inputs to function.

--- R/tausquared_HBEST.R
+++ R/tausquared_HBEST.R
@@ -18,7 +18,7 @@
 #' @export
 #'
 #' @examples
-tausquared_HBEST = function(ebr, ab, B, D, R, cur_zetasquared, nu_tau, tau_min, tau_max, num_gpts){
+tausquared_HBEST = function(loc, glob, B, D, R, cur_zetasquared, nu_tau, tau_min, tau_max, num_gpts){
   p_min = pt(q = tau_min, df = nu_tau)
   p_max = pt(q = tau_max, df = nu_tau)
   p_grid = seq(from = p_min, to = p_max,length.out = num_gpts)
@@ -27,8 +27,8 @@ tausquared_HBEST = function(ebr, ab, B, D, R, cur_zetasquared, nu_tau, tau_min, 
   # non-linear transformation of the grid
   tausquared_grid = tau_grid^2
   # compute log posterior-probability at each grid point
-  lgprob = .5*-B*(R+1) * log(tausquared_grid) - sum(colSums(ebr^2 / D) / (cur_zetasquared - 1)) / (2*tausquared_grid) -
-    sum(ab^2 / D) / (2*tausquared_grid) - (nu_tau + 1) / 2 * log(1 + tau_grid / nu_tau)       
+  lgprob = .5*-B*(R+1) * log(tausquared_grid) - sum(colSums(loc^2 / D) / (cur_zetasquared - 1)) / (2*tausquared_grid) -
+    sum(glob^2 / D) / (2*tausquared_grid) - (nu_tau + 1) / 2 * log(1 + tau_grid / nu_tau)       
   lgprob = lgprob - max(lgprob)
   # Get back into probability scale (softmax)
   postprob = exp(lgprob)/sum(exp(lgprob))
