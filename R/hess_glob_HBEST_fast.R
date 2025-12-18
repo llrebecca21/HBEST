4b0daa3ecfabb770d1a26fcb4241f49acfc3f709
llrebecca21 <llrebecca21@gmail.com>
2025-12-18 12:15:43 -0800

    Update variable inputs and documentation.

--- R/hess_glob_HBEST_fast.R
+++ R/hess_glob_HBEST_fast.R
@@ -2,21 +2,22 @@
 #'
 #' @param Psi_list 
 #' @param y_list 
-#' @param ebr 
-#' @param ab 
-#' @param Sigma_e 
 #' @param R 
+#' @param glob 
+#' @param loc 
+#' @param Sigma_glob 
+#' @param Psi_loc_list 
 #'
 #' @description
 #' `hess_glob_HBEST_fast` calculates the hessian of the conditional posterior of the `r`th \eqn{beta} for model A.
 #'
 #' @return
 #' @noRd
-hess_glob_HBEST_fast <- function(ab, Psi_list, y_list, ebr, Sigma_a, R, Psi_ebr_list) {
-  ha = diag(-1/Sigma_a)
+hess_glob_HBEST_fast <- function(glob, Psi_list, y_list, loc, Sigma_glob, R, Psi_loc_list) {
+  ha = diag(-1/Sigma_glob)
   for(r in 1:R){
     # ha = ha - crossprod(Psi_list[[r]], Psi_list[[r]] * c(y_list[[r]] / exp(Psi_list[[r]] %*% (ebr[,r] + ab))))
-    ha = ha - crossprod(Psi_list[[r]], Psi_list[[r]] * c(y_list[[r]] / exp(Psi_list[[r]] %*% ab + Psi_ebr_list[[r]])))
+    ha = ha - crossprod(Psi_list[[r]], Psi_list[[r]] * c(y_list[[r]] / exp(Psi_list[[r]] %*% glob + Psi_loc_list[[r]])))
   }
   return(ha)
 }
