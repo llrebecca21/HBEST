c35c5541b0a83073fae3f5ea62622ce7ae2f29bf
llrebecca21 <llrebecca21@gmail.com>
2025-12-18 12:11:29 -0800

    Rename file to match updated function name.

--- R/grad_glob_HBEST_fast.R
+++ R/grad_glob_HBEST_fast.R
@@ -1,7 +1,7 @@
 #' Calculate the gradient of the conditional log posterior distribution of \eqn{\pmb{a}} from Model A.
 #' 
 #' @description
-#' `gradienta_modelA_fast` function calculates the conditional posterior for the `r`th \eqn{\beta} coefficients under Model B.
+#' `grad_glob_HBEST_fast` function calculates the conditional posterior for the `r`th \eqn{\beta} coefficients under Model B.
 #' 
 #' @param sumPsi  ((B+1) x R) The `r`th column of the column sum across `Psi`.
 #' @param ebr 
@@ -13,7 +13,7 @@
 #'
 #' @return A vector containing the gradient for the conditional posterior of \eqn{\beta}.
 #'
-gradienta_modelA_fast <- function(ab, sumPsi, Psi_list, y_list, Sigma_a, R, sumsumPsi, Psi_ebr_list) {
+grad_glob_HBEST_fast <- function(ab, sumPsi, Psi_list, y_list, Sigma_a, R, sumsumPsi, Psi_ebr_list) {
   k = -sumsumPsi - (ab / Sigma_a) 
   for(r in 1:R){
     # k = k + colSums(Psi_list[[r]] * c(y_list[[r]] / exp(Psi_list[[r]] %*% (ab + ebr[,r]))))
