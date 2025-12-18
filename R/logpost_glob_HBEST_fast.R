eb103be01ffcf263e5ccd2136f22cef0ead84b5b
llrebecca21 <llrebecca21@gmail.com>
2025-12-18 12:43:22 -0800

    Update the variables, function names, file names, and input variables.

--- R/logpost_glob_HBEST_fast.R
+++ R/logpost_glob_HBEST_fast.R
@@ -1,7 +1,7 @@
 #' Function that calculates the conditional log posterior distribution of the vector \eqn{\pmb{a}} for Model A.
 #'
 #' @description
-#' `logposteriora_modelA` calculates the conditional posterior distribution for model B
+#' `logpost_glob_HBEST_fast` calculates the conditional posterior distribution for model B
 #'
 #' @param ab 
 #' @param ebr 
@@ -12,12 +12,12 @@
 #'
 #' @return A vector that contains the `r`th posterior conditional for \eqn{\beta^{(r)}}.
 #' @noRd
-logposteriora_modelA_fast = function(ab, Sigma_a, y_list, sumPsi, Psi_list, R, sumsumPsi, Psi_ebr_list){
+logpost_glob_HBEST_fast = function(glob, Sigma_glob, y_list, sumPsi, Psi_list, R, sumsumPsi, Psi_loc_list){
  #k = -c(crossprod(sumsumPsi, ab)) - sum(ab^2 / (Sigma_a * 2))
-  k = -sum(sumsumPsi * ab) - sum(ab^2 / (Sigma_a * 2))
+  k = -sum(sumsumPsi * glob) - sum(glob^2 / (Sigma_glob * 2))
   for(r in 1:R){
     # k = k-sum(y_list[[r]] / exp(Psi_list[[r]] %*% (ab + ebr[,r])))
-    k = k-sum(y_list[[r]] / exp(Psi_list[[r]] %*% (ab) + Psi_ebr_list[[r]]))
+    k = k-sum(y_list[[r]] / exp(Psi_list[[r]] %*% (glob) + Psi_loc_list[[r]]))
   }
   return(k)
 }
