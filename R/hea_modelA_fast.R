ba13a0a25d5fadf2db2ee17d5c0670839ad2dec7
llrebecca21 <llrebecca21@gmail.com>
2025-12-18 11:32:27 -0800

    Update comments and documentation.

--- R/hea_modelA_fast.R
+++ R/hea_modelA_fast.R
@@ -1,4 +1,4 @@
-#' Hessian function for conditional log posterior of \eqn{\pmb{a}} from Model A
+#' Hessian function for conditional log posterior of \eqn{\pmb{glob}} from HBEST
 #'
 #' @param Psi_list 
 #' @param y_list 
@@ -14,7 +14,7 @@
 #' @return
 #'
 #' @examples
-hea_modelA_fast <- function( ab, Psi_list, y_list, ebr, Sigma_a, R, Psi_ebr_list) {
+hea_modelA_fast <- function(ab, Psi_list, y_list, ebr, Sigma_a, R, Psi_ebr_list) {
   ha = diag(-1/Sigma_a)
   for(r in 1:R){
     # ha = ha - crossprod(Psi_list[[r]], Psi_list[[r]] * c(y_list[[r]] / exp(Psi_list[[r]] %*% (ebr[,r] + ab))))
