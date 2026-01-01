761c8c3caa0c4d99f30b444b01a5e59c763d9e15
llrebecca21 <llrebecca21@gmail.com>
2026-01-01 11:54:26 -0600

    Update documentation and style the file.

--- R/gen_data.R
+++ R/gen_data.R
@@ -1,9 +1,10 @@
-#' @title Time-series Data Generation Wrapper Function
-#' 
+#' @title Time-series data generation wrapper function
+#'
 #' @description
-#' This function is a wrapper function for various time-series data generation methods.
-#' This is used to call one of six methods that generates data in the structure used for simulations for [HBEST::HBEST()] or [HBEST::HBEST_fast()].
-#' 
+#' `data_gen()` is a wrapper function for various time-series data generation methods.
+#' This is used to call one of six methods that generates data in the structure required for
+#' simulations for [HBEST::HBEST()] or [HBEST::HBEST_fast()].
+#'
 #' @param gen_method Method of data generation to use. Options include `ARp`, `AR1vary`, `AR2mix`, `MA4`, `MA4vary`, and `MA4varyType2`.
 #' @param ... further arguments, passed to chosen method. See details below.
 #'
@@ -16,99 +17,108 @@
 #'   `MA4vary` \tab calls the [HBEST::generate_MA4_vary()] function which is based on [stats::arima.sim]. \cr
 #'   `MA4varyType2` \tab calls the [HBEST::generate_MA4_varyType2()] function which is based on [stats::arima.sim]. \cr
 #' }
-#' 
+#'
 #' @returns The `gen_data()` function returns the appropriate objects based on the method chosen. See help pages for each function for more details.
-#' @export
 #'
 #' @examples
-#' R = 20
-#' n = 500
-#' burn = 50
-#' 
+#' R <- 20
+#' n <- 500
+#' burn <- 50
+#'
 #' ## `ARp` method:
-#' ts = gen_data(gen_method = "ARp",
-#'  phi = c(0.5),
-#'  n = n,
-#'  R = R,
-#'  burn = burn)
-#' 
+#' ts <- gen_data(
+#'   gen_method = "ARp",
+#'   phi = c(0.5),
+#'   n = n,
+#'   R = R,
+#'   burn = burn
+#' )
+#'
 #' ## `AR1vary` method:
-#' ts = gen_data(gen_method = "AR1vary",
-#'  n = n,
-#'  R = 5,
-#'  min = 0.45,
-#'  max = 0.6,
-#'  burn = burn)
-#' 
+#' ts <- gen_data(
+#'   gen_method = "AR1vary",
+#'   n = n,
+#'   R = 5,
+#'   min = 0.45,
+#'   max = 0.6,
+#'   burn = burn
+#' )
+#'
 #' ## `AR2mix` method:
-#' R = 20
-#' peaks1 = runif(R, min = 0.2, max = 0.23)
-#' bandwidths1 = runif(R, min = .1, max = .2)
-#' peaks2 = runif(R, min = (pi * (2/5)) - 0.1, max = (pi * (2/5)) + 0.1)
-#' bandwidths2 = rep(0.15, R)
-#' peaks = rbind(peaks1, peaks2)
-#' bandwidths = rbind(bandwidths1, bandwidths2)
-#' ts = gen_data(gen_method = "AR2mix",
-#'  peaks = peaks,
-#'  bandwidths = bandwidths,
-#'  n_vary = c(rep(300, 10),
-#'  rep(800, 10)))
-#' 
+#' R <- 20
+#' peaks1 <- runif(R, min = 0.2, max = 0.23)
+#' bandwidths1 <- runif(R, min = .1, max = .2)
+#' peaks2 <- runif(R, min = (pi * (2 / 5)) - 0.1, max = (pi * (2 / 5)) + 0.1)
+#' bandwidths2 <- rep(0.15, R)
+#' peaks <- rbind(peaks1, peaks2)
+#' bandwidths <- rbind(bandwidths1, bandwidths2)
+#' ts <- gen_data(
+#'   gen_method = "AR2mix",
+#'   peaks = peaks,
+#'   bandwidths = bandwidths,
+#'   n_vary = c(
+#'     rep(300, 10),
+#'     rep(800, 10)
+#'   )
+#' )
+#'
 #' ## `MA4` method:
-#' n = rep(500, R)
-#' ts = gen_data(gen_method = "MA4",
-#'  n = n,
-#'  R = R,
-#'  burn = burn)
-#' 
+#' n <- rep(500, R)
+#' ts <- gen_data(
+#'   gen_method = "MA4",
+#'   n = n,
+#'   R = R,
+#'   burn = burn
+#' )
+#'
 #' ## `MA4vary` method:
-#' n = rep(500, R)
-#' alpha = 0.05
-#' ts = gen_data(gen_method = "MA4vary",
-#'  n = n,
-#'  R = R,
-#'  burn = burn,
-#'  alpha = alpha)
-#' 
+#' n <- rep(500, R)
+#' alpha <- 0.05
+#' ts <- gen_data(
+#'   gen_method = "MA4vary",
+#'   n = n,
+#'   R = R,
+#'   burn = burn,
+#'   alpha = alpha
+#' )
+#'
 #' ## `MA4varyType2` method:
-#' n = rep(500, R)
-#' ts = gen_data(gen_method = "MA4varyType2",
-#'  n = n,
-#'  R = R,
-#'  burn = burn,
-#'  alpha = alpha)
-#' 
-gen_data = function(gen_method, ...){
-  gen_method = match.arg(gen_method, c("ARp", "AR1vary", "AR2mix", "MA4","MA4vary","MA4varyType2"))
+#' n <- rep(500, R)
+#' ts <- gen_data(
+#'   gen_method = "MA4varyType2",
+#'   n = n,
+#'   R = R,
+#'   burn = burn,
+#'   alpha = alpha
+#' )
+#'
+#' @export
+gen_data <- function(gen_method, ...) {
+  gen_method <- match.arg(gen_method, c("ARp", "AR1vary", "AR2mix", "MA4", "MA4vary", "MA4varyType2"))
   # Extract call parameters:
-  Call = c(as.list(environment()), list(...))
-  
-  Send_Call = switch(gen_method,
-                     ARp = formals(generate_AR),
-                     AR1vary = formals(generate_AR1_vary),
-                     AR2mix = formals(generate_AR2_mixture),
-                     MA4vary = formals(generate_MA4_vary),
-                     MA4 = formals(generate_MA4),
-                     MA4varyType2 = formals(generate_MA4_varyType2),
-                     stop("Invalid `gen_method` value"))
-  
-  Send_Call[names(Call)[names(Call) %in% names(Send_Call)]] = Call[names(Call) %in% names(Send_Call)]
-  
-  out = switch(gen_method,
-               ARp = do.call(generate_AR, args = Send_Call),
-               AR1vary = do.call(generate_AR1_vary, args = Send_Call),
-               AR2mix = do.call(generate_AR2_mixture, args = Send_Call),
-               MA4vary = do.call(generate_MA4_vary, args = Send_Call),
-               MA4 = do.call(generate_MA4, args = Send_Call),
-               MA4varyType2 = do.call(generate_MA4_varyType2, args = Send_Call),
-               stop("Invalid `gen_method` value"))
-  
+  Call <- c(as.list(environment()), list(...))
+
+  Send_Call <- switch(gen_method,
+    ARp = formals(generate_AR),
+    AR1vary = formals(generate_AR1_vary),
+    AR2mix = formals(generate_AR2_mixture),
+    MA4vary = formals(generate_MA4_vary),
+    MA4 = formals(generate_MA4),
+    MA4varyType2 = formals(generate_MA4_varyType2),
+    stop("Invalid `gen_method` value")
+  )
+
+  Send_Call[names(Call)[names(Call) %in% names(Send_Call)]] <- Call[names(Call) %in% names(Send_Call)]
+
+  out <- switch(gen_method,
+    ARp = do.call(generate_AR, args = Send_Call),
+    AR1vary = do.call(generate_AR1_vary, args = Send_Call),
+    AR2mix = do.call(generate_AR2_mixture, args = Send_Call),
+    MA4vary = do.call(generate_MA4_vary, args = Send_Call),
+    MA4 = do.call(generate_MA4, args = Send_Call),
+    MA4varyType2 = do.call(generate_MA4_varyType2, args = Send_Call),
+    stop("Invalid `gen_method` value")
+  )
+
   return(out)
-  
 }
-
-
-
-
-
-
